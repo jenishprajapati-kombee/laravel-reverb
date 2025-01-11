dd326517c2bef8abfb2e1dcecf0cecbf05f9a4e4
Jenish Prajapati <jenish.prajapati@easternts.com>
2025-01-11 11:53:54 +0530

    Remove unnecessary line breaks in real-time message form
    Removed the extra line breaks after the input field in the real-time message form to improve the layout and maintain consistency.

--- resources/views/livewire/real-time-message.blade.php
+++ resources/views/livewire/real-time-message.blade.php
@@ -4,7 +4,7 @@
             <div class="col">
                 <form class="form" wire:submit.prevent='triggerEvent'>
                     <label for="fname">Send Realtime Message</label><br>
-                    <input type="text" wire:model='message' class="form-control"><br><br>
+                    <input type="text" wire:model='message' class="form-control">
                     <input type="submit" class="btn btn-primary mt-3">
                 </form>
             </div>
